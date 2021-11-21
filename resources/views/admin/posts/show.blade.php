cfc835ea26f67885736e87f0461ceae043512f9b
WalterFrongia <walter.frongia@libero.it>
2021-11-21 17:11:03 +0100

    Aggiunto un file css per modificare il file show.blade e rappresentati i dati graficamente

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.main')
 
 @section('posts')
-    <h1>show</h1>
+    <div id="show-container">
+        <h2>{{$post->title}}</h2>
+       <img src="{{$post->url_img}}" alt="">
+       <p>{{$post->text}}</p>
+       <p>{{$post->publication_date}}</p>
+    </div>
 @endsection
