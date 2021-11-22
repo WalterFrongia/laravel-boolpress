ad9005f5893c1b53298b59e85b4799a422a2fc90
WalterFrongia <walter.frongia@libero.it>
2021-11-22 15:56:37 +0100

    Aggiunto namespace in post model in fnction category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,7 +9,7 @@ class Post extends Model
     protected $fillable = ['title' , 'img_url' , 'text' , 'publication_date'];
 
     public function category() {
-        return $this->belongsTo(App\Models\Category);      
+        return $this->belongsTo('App\Models\Category');      
     }
 
     public function tags(){
